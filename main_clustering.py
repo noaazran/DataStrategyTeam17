33b33f1c1ad8ba1bf32968fb57ebbbe16a5afb09
hubertdepsv <60505590+hubertdepsv@users.noreply.github.com>
2022-02-10 08:36:34 +0100

    Kmeans clustering script

---
+++ main_clustering.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import LabelEncoder
+from kneed import KneeLocator
+
+from src.clustering import *
+
+datatypes = {
+    'product_id': 'uint32',
+    'client_id': 'uint32',
+    'sales_net': 'float64',
+    'quantity': 'int32',
+    'order_channel': 'object',
+    'branch_id': 'uint16',
+    'unit_price': 'int8',
+    'stock_flow': 'int8',
+    'month_order': 'uint8',
+    'order_invoice_delta': 'float16'
+}
+
+def main():
+    print("Loading preprocesse dataset...")
+    df = load_preprocessed_data(datatypes)
+    
+    print("Encoding categorical column...")
+    df = categorical_encoding(df)
+    df = drop_na(df)
+
+    print("Clustering...")
+    df_kmeans = kmeans_clustering(df, min_n=2, max_n=8)
+    
+    print("Saving cleaned data set...")
+    df_kmeans.to_csv(os.path.join("data", "kmeans_data.csv"))
+    print("Done")
+    return
+
+
+if __name__=="__main__":
+    main()
---
+++ src/clustering.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import LabelEncoder
+from kneed import KneeLocator
+
+def load_preprocessed_data(datatypes):
+    return pd.read_csv('data/preprocessed_data.csv', dtype=datatypes, sep=',').drop(columns=['Unnamed: 0', 'date_order', 'date_invoice'], axis=1)
+
+
+def categorical_encoding(df):
+    le = LabelEncoder()
+    df.order_channel = le.fit_transform(df.order_channel)
+    return df
+
+def drop_na(df):
+    return df.dropna(inplace=True)
+
+
+def kmeans_clustering(df, min_n=2, max_n=8):
+    """
+    Description:
+    creates clusters using k-means
+    --------------------------------------------
+    Args:
+        df: input dataframe
+        min_n: max number of clusters in the range(min, max) of KneeLocator
+        max_n: max number of clusters in the range(min, max) of KneeLocator
+
+    Returns: a dataframe with clusters
+    """
+    kmeans_kwargs = {
+            "init": "random",
+            "n_init": 10,
+            "max_iter": 750,
+            "random_state": 0
+        }
+    # A list holds the SSE values for each k
+    sse = []
+    for k in range(min_n, max_n):
+        kmeans = KMeans(n_clusters=k, **kmeans_kwargs)
+        kmeans.fit(df)
+        sse.append(kmeans.inertia_)
+
+    kl = KneeLocator(
+        range(min_n, max_n), sse, curve="convex", direction="decreasing"
+    )
+
+    kmeans = KMeans(n_clusters=kl.elbow, **kmeans_kwargs).fit(df)
+
+    df_kmeans = pd.DataFrame(kmeans.transform(df))
+    df_kmeans.columns = [str(i) for i in range(kl.elbow)]
+    df_kmeans['cluster'] = kmeans.predict(df)
+    return df_kmeans
